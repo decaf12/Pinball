b992280159c1c4e9f2e4c51698553399fdedacce
decaf <siyang.he.uw@gmail.com>
2023-02-28 13:41:07 -0500

    bare minimum

---
+++ Assets/Scripts/PlungerScript.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlungerScript : MonoBehaviour
+{
+    private float power;
+    private float minPower = 0f;
+    public float maxPower = 100f;
+    public Slider powerSlider;
+    List<Rigidbody> ballList;
+    private bool ballReady;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerSlider.minValue = 0f;
+        powerSlider.maxValue = maxPower;
+        ballList = new List<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        powerSlider.gameObject.SetActive(ballReady);
+        
+        powerSlider.value = power;
+
+        if (ballList.Count > 0)
+        {
+            ballReady = true;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (power <= maxPower)
+                {
+                    power += 50 * Time.deltaTime;
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                foreach (Rigidbody r in ballList)
+                {
+                    r.AddForce(power * Vector3.forward);
+                }
+            }
+
+        }
+        else
+        {
+            ballReady = false;
+            power = 0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            ballList.Add(other.gameObject.GetComponent<Rigidbody>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            ballList.Remove(other.gameObject.GetComponent<Rigidbody>());
+            power = 0f;
+        }
+    }
+}
